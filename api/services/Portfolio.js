3d9839be79357da1b7322f4775518887eca2784d
Pooja <pthakare33@gmail.com>
2016-05-26 19:01:11 +0530

    beautify kia

--- api/services/Portfolio.js
+++ api/services/Portfolio.js
@@ -2,169 +2,226 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-  status: { type: Boolean, default:false },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    index: true
-  },
-  funds: [{
-    fundid: { type: String, default:"" },
-    purchaseprice: { type: String, default:"" },
-    quantity: { type: String, default:"" }
-  }],
-  goalName: { type: String, default:"" },
-  lumpsum: { type: Number, default:0 },
-  monthly: { type: Number, default:0 },
-  noOfMonth: { type: Number, default:0 },
-  withDrawalFreq: { type: String, default:"" },
-  inflation: { type: Number, default:0 },
-  installment: { type: Number, default:0 },
-  noOfInstallment: { type: Number, default:0 },
-  startMonth: { type: Number, default:0 },
-  shortinput: { type: Number, default:0 },
-  longinput: { type: Number, default:0 }
+    status: {
+        type: Boolean,
+        default: false
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        index: true
+    },
+    funds: [{
+        fundid: {
+            type: String,
+            default: ""
+        },
+        purchaseprice: {
+            type: String,
+            default: ""
+        },
+        quantity: {
+            type: String,
+            default: ""
+        }
+    }],
+    goalName: {
+        type: String,
+        default: ""
+    },
+    lumpsum: {
+        type: Number,
+        default: 0
+    },
+    monthly: {
+        type: Number,
+        default: 0
+    },
+    noOfMonth: {
+        type: Number,
+        default: 0
+    },
+    withDrawalFreq: {
+        type: String,
+        default: ""
+    },
+    inflation: {
+        type: Number,
+        default: 0
+    },
+    installment: {
+        type: Number,
+        default: 0
+    },
+    noOfInstallment: {
+        type: Number,
+        default: 0
+    },
+    startMonth: {
+        type: Number,
+        default: 0
+    },
+    shortinput: {
+        type: Number,
+        default: 0
+    },
+    longinput: {
+        type: Number,
+        default: 0
+    }
 });
 
 module.exports = mongoose.model('Portfolio', schema);
 
 var models = {
-  saveData: function(data, callback) {
-    var portfolio = this(data);
-    portfolio.timestamp = new Date();
-    if (data._id) {
-      this.findOneAndUpdate({
-        _id: data._id
-      }, data).exec(function(err, updated) {
-        if (err) {
-          console.log(err);
-          callback(err, null);
-        } else if (updated) {
-          callback(null, updated);
+    saveData: function(data, callback) {
+        var portfolio = this(data);
+        portfolio.timestamp = new Date();
+        if (data._id) {
+            this.findOneAndUpdate({
+                _id: data._id
+            }, data).exec(function(err, updated) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                } else if (updated) {
+                    callback(null, updated);
+                } else {
+                    callback(null, {});
+                }
+            });
         } else {
-          callback(null, {});
+            portfolio.save(function(err, created) {
+                if (err) {
+                    callback(err, null);
+                } else if (created) {
+                    callback(null, created);
+                } else {
+                    callback(null, {});
+                }
+            });
         }
-      });
-    } else {
-      portfolio.save(function(err, created) {
-        if (err) {
-          callback(err, null);
-        } else if (created) {
-          callback(null, created);
-        } else {
-          callback(null, {});
-        }
-      });
-    }
-  },
-  deleteData: function(data, callback) {
-    this.findOneAndRemove({
-      _id: data._id
-    }, function(err, deleted) {
-      if (err) {
-        callback(err, null);
-      } else if (deleted) {
-        callback(null, deleted);
-      } else {
-        callback(null, {});
-      }
-    });
-  },
-  getAll: function(data, callback) {
-    this.find({}).exec(function(err, found) {
-      if (err) {
-        console.log(err);
-        callback(err, null);
-      } else if (found && found.length > 0) {
-        callback(null, found);
-      } else {
-        callback(null, []);
-      }
-    });
-  },
-  getOne: function(data, callback) {
-    this.findOne({
-      "_id": data._id
-    }).exec(function(err, found) {
-      if (err) {
-        console.log(err);
-        callback(err, null);
-      } else if (found && Object.keys(found).length > 0) {
-        callback(null, found);
-      } else {
-        callback(null, {});
-      }
-    });
-  },
+    },
+    deleteData: function(data, callback) {
+        this.findOneAndRemove({
+            _id: data._id
+        }, function(err, deleted) {
+            if (err) {
+                callback(err, null);
+            } else if (deleted) {
+                callback(null, deleted);
+            } else {
+                callback(null, {});
+            }
+        });
+    },
+    getAll: function(data, callback) {
+        this.find({}).exec(function(err, found) {
+            if (err) {
+                console.log(err);
+                callback(err, null);
+            } else if (found && found.length > 0) {
+                callback(null, found);
+            } else {
+                callback(null, []);
+            }
+        });
+    },
+    getOne: function(data, callback) {
+        this.findOne({
+            "_id": data._id
+        }).populate([{
+            path: "user",
+            select: {
+                _id: 1,
+                name: 1
+            }
+        }]).populate([{
+            path: "funds",
+            select: {
+                _id: 1,
+                name: 1
+            }
+        }]).exec(function(err, found) {
+            if (err) {
+                console.log(err);
+                callback(err, null);
+            } else if (found && Object.keys(found).length > 0) {
+                callback(null, found);
+            } else {
+                callback(null, {});
+            }
+        });
+    },
 
-  getPortfolio: function(data, callback){
-    this.find({
-      "user": data.user
-    }).exec(function(err, found) {
-      if (err) {
-        console.log(err);
-        callback(err, null);
-      } else if (found && Object.keys(found).length > 0) {
-        callback(null, found);
-      } else {
-        callback(null, {});
-      }
-    });
-  },
-  findLimited: function(data, callback) {
-    var newreturns = {};
-    newreturns.data = [];
-    var check = new RegExp(data.search, "i");
-    data.pagenumber = parseInt(data.pagenumber);
-    data.pagesize = parseInt(data.pagesize);
-    async.parallel([
-        function(callback) {
-          Portfolio.count({
-            goalname: {
-              '$regex': check
-            }
-          }).exec(function(err, number) {
+    getPortfolio: function(data, callback) {
+        this.find({
+            "user": data.user
+        }).exec(function(err, found) {
             if (err) {
-              console.log(err);
-              callback(err, null);
-            } else if (number && number !== "") {
-              newreturns.total = number;
-              newreturns.totalpages = Math.ceil(number / data.pagesize);
-              callback(null, newreturns);
+                console.log(err);
+                callback(err, null);
+            } else if (found && Object.keys(found).length > 0) {
+                callback(null, found);
             } else {
-              callback(null, newreturns);
+                callback(null, {});
             }
-          });
-        },
-        function(callback) {
-          Portfolio.find({
-            goalname: {
-              '$regex': check
-            }
-          }).skip(data.pagesize * (data.pagenumber - 1)).limit(data.pagesize).exec(function(err, data2) {
-            if (err) {
-              console.log(err);
-              callback(err, null);
-            } else if (data2 && data2.length > 0) {
-              newreturns.data = data2;
-              callback(null, newreturns);
-            } else {
-              callback(null, newreturns);
-            }
-          });
-        }
-      ],
-      function(err, data4) {
-        if (err) {
-          console.log(err);
-          callback(err, null);
-        } else if (data4) {
-          callback(null, newreturns);
-        } else {
-          callback(null, newreturns);
-        }
-      });
-  }
+        });
+    },
+    findLimited: function(data, callback) {
+        var newreturns = {};
+        newreturns.data = [];
+        var check = new RegExp(data.search, "i");
+        data.pagenumber = parseInt(data.pagenumber);
+        data.pagesize = parseInt(data.pagesize);
+        async.parallel([
+                function(callback) {
+                    Portfolio.count({
+                        goalname: {
+                            '$regex': check
+                        }
+                    }).exec(function(err, number) {
+                        if (err) {
+                            console.log(err);
+                            callback(err, null);
+                        } else if (number && number !== "") {
+                            newreturns.total = number;
+                            newreturns.totalpages = Math.ceil(number / data.pagesize);
+                            callback(null, newreturns);
+                        } else {
+                            callback(null, newreturns);
+                        }
+                    });
+                },
+                function(callback) {
+                    Portfolio.find({
+                        goalname: {
+                            '$regex': check
+                        }
+                    }).skip(data.pagesize * (data.pagenumber - 1)).limit(data.pagesize).exec(function(err, data2) {
+                        if (err) {
+                            console.log(err);
+                            callback(err, null);
+                        } else if (data2 && data2.length > 0) {
+                            newreturns.data = data2;
+                            callback(null, newreturns);
+                        } else {
+                            callback(null, newreturns);
+                        }
+                    });
+                }
+            ],
+            function(err, data4) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                } else if (data4) {
+                    callback(null, newreturns);
+                } else {
+                    callback(null, newreturns);
+                }
+            });
+    }
 };
 
 module.exports = _.assign(module.exports, models);
